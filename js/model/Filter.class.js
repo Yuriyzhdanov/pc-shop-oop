39fddc7c1520263527d357be0a13d9027a7b20ce
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2024-08-24 17:46:35 +0300

    implement data sorting in Filter.model

--- js/model/Filter.class.js
+++ js/model/Filter.class.js
@@ -10,6 +10,8 @@ class Filter {
         if (!this[key].includes(value)) {
           this[key].push(value)
         }
+        this[key] = normalizeStorageCapacity(this[key])
+        this[key] = sortAttrs(this[key], key)
       }
     }
   }
@@ -19,39 +21,37 @@ class Filter {
   }
 }
 
-// modelFilter[key] = normalizeStorageCapacity(modelFilter[key])
-// modelFilter[key] = sortAttrs(modelFilter[key], key)
+function sortAttrs(array, type) {
+  switch (type) {
+    case 'Количество ядер':
+    case 'Объем ОЗУ':
+      return array.sort((a, b) => parseInt(a) - parseInt(b))
+    case 'Объем накопителя':
+    case 'Блок питания':
+    case 'Частота ОЗУ':
+      return array.sort(
+        (a, b) =>
+          parseInt(a.replace(/[^0-9]/g, '')) -
+          parseInt(b.replace(/[^0-9]/g, ''))
+      )
+    case 'Частота процессора':
+      return array.sort(
+        (a, b) =>
+          parseFloat(a.replace(/[^\d.]/g, '')) -
+          parseFloat(b.replace(/[^\d.]/g, ''))
+      )
+    case 'Тип ОЗУ':
+    case 'Тип накопителя':
+    case 'Процессор':
+    case 'Материнская плата':
+      return array.sort()
+    default:
+      return array.sort()
+  }
+}
 
-// function sortAttrs(array, type) {
-//   switch (type) {
-//     case 'Количество ядер':
-//     case 'Объем ОЗУ':
-//       return array.sort((a, b) => parseInt(a) - parseInt(b))
-//     case 'Объем накопителя':
-//     case 'Блок питания':
-//     case 'Частота ОЗУ':
-//       return array.sort(
-//         (a, b) =>
-//           parseInt(a.replace(/[^0-9]/g, '')) -
-//           parseInt(b.replace(/[^0-9]/g, ''))
-//       )
-//     case 'Частота процессора':
-//       return array.sort(
-//         (a, b) =>
-//           parseFloat(a.replace(/[^\d.]/g, '')) -
-//           parseFloat(b.replace(/[^\d.]/g, ''))
-//       )
-//     case 'Тип ОЗУ':
-//     case 'Тип накопителя':
-//     case 'Процессор':
-//     case 'Материнская плата':
-//       return array.sort()
-//     default:
-//       return array.sort()
-//   }
-// }
+function normalizeStorageCapacity(storageCapacity) {
+  return storageCapacity.map(capacity => capacity.replace(/,$/, ''))
+}
 
-// function normalizeStorageCapacity(storageCapacity) {
-//   return storageCapacity.map(capacity => capacity.replace(/,$/, ''))
-// }
 export default Filter
