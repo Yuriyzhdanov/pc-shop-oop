9d2d8eb8f33790c4d5cdc4698573a24c0acfe1eb
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2024-08-24 17:53:07 +0300

    move func from Filter.js to utils.js

--- js/model/Filter.class.js
+++ js/model/Filter.class.js
@@ -1,3 +1,5 @@
+import { sortAttrs, normalizeStorageCapacity } from './utils.js'
+
 class Filter {
   update(products) {
     const specs = products.map(product => product.attributes)
@@ -19,39 +21,6 @@ class Filter {
   clear() {
     Object.keys(this).forEach(prop => delete this[prop])
   }
-}
-
-function sortAttrs(array, type) {
-  switch (type) {
-    case 'Количество ядер':
-    case 'Объем ОЗУ':
-      return array.sort((a, b) => parseInt(a) - parseInt(b))
-    case 'Объем накопителя':
-    case 'Блок питания':
-    case 'Частота ОЗУ':
-      return array.sort(
-        (a, b) =>
-          parseInt(a.replace(/[^0-9]/g, '')) -
-          parseInt(b.replace(/[^0-9]/g, ''))
-      )
-    case 'Частота процессора':
-      return array.sort(
-        (a, b) =>
-          parseFloat(a.replace(/[^\d.]/g, '')) -
-          parseFloat(b.replace(/[^\d.]/g, ''))
-      )
-    case 'Тип ОЗУ':
-    case 'Тип накопителя':
-    case 'Процессор':
-    case 'Материнская плата':
-      return array.sort()
-    default:
-      return array.sort()
-  }
-}
-
-function normalizeStorageCapacity(storageCapacity) {
-  return storageCapacity.map(capacity => capacity.replace(/,$/, ''))
 }
 
 export default Filter
--- js/model/utils.js
+++ js/model/utils.js
@@ -10,4 +10,37 @@ function setWithLimits(newVal, max, min) {
   return Math.max(LIMIT_MIN, Math.min(newVal, LIMIT_MAX))
 }
 
-export { checkNumber, setWithLimits }
+function sortAttrs(array, type) {
+  switch (type) {
+    case 'Количество ядер':
+    case 'Объем ОЗУ':
+      return array.sort((a, b) => parseInt(a) - parseInt(b))
+    case 'Объем накопителя':
+    case 'Блок питания':
+    case 'Частота ОЗУ':
+      return array.sort(
+        (a, b) =>
+          parseInt(a.replace(/[^0-9]/g, '')) -
+          parseInt(b.replace(/[^0-9]/g, ''))
+      )
+    case 'Частота процессора':
+      return array.sort(
+        (a, b) =>
+          parseFloat(a.replace(/[^\d.]/g, '')) -
+          parseFloat(b.replace(/[^\d.]/g, ''))
+      )
+    case 'Тип ОЗУ':
+    case 'Тип накопителя':
+    case 'Процессор':
+    case 'Материнская плата':
+      return array.sort()
+    default:
+      return array.sort()
+  }
+}
+
+function normalizeStorageCapacity(storageCapacity) {
+  return storageCapacity.map(capacity => capacity.replace(/,$/, ''))
+}
+
+export { setWithLimits, sortAttrs, normalizeStorageCapacity }
