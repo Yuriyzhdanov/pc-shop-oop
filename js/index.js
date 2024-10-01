d1b622b4e8035fdb6d6fc3000b2857db8117e94b
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2024-10-01 19:08:27 +0300

    fix path index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 import controllerCatalog from './controller/controllerCatalog.js'
 import controllerFavorites from './controller/controllerFavorites.js'
-// import controllerProduct from './controller/controllerProduct.js'
-// import controllerCart from './controller/controllerCart.js'
+import controllerProduct from './controller/controllerProduct.js'
+import controllerCart from './controller/controllerCart.js'
+import controllerCompare from './controller/controllerCompare.js'
 
 document.addEventListener('DOMContentLoaded', onDOMContentLoadedDocument)
 
@@ -12,11 +13,13 @@ async function onDOMContentLoadedDocument() {
   if (location.pathname.includes('favorites.html')) {
     await controllerFavorites.handleDOMContentLoaded()
   }
-  // if (location.pathname.includes('product.html')) {
-  //   await controllerProduct.handleDOMContentLoaded()
-  // }
-  // if (location.pathname.includes('cart.html')) {
-  //   await controllerCart.handleDOMContentLoaded()
-  // }
+  if (location.pathname.includes('product.html')) {
+    await controllerProduct.handleDOMContentLoaded()
+  }
+  if (location.pathname.includes('cart.html')) {
+    await controllerCart.handleDOMContentLoaded()
+  }
+  if (location.pathname.includes('compare.html')) {
+    await controllerCompare.handleDOMContentLoaded()
+  }
 }
-window.superYurii = 'ok'
